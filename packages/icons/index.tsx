067476765fd2c8901d76675657e9d825a0ba3dc7
Nuno Frade <nfrade.tribal@gmail.com>
2021-01-19 17:26:10 +0100

    More icons

--- packages/icons/index.tsx
+++ packages/icons/index.tsx
@@ -5,18 +5,31 @@ import React, {
   FunctionComponent,
 } from 'react'
 
+import AddSvg from './Components/Add'
 import ChevronLeftSvg from './Components/ChevronLeft'
+import ChevronRightSvg from './Components/ChevronRight'
+import CollapseSvg from './Components/Collapse'
 import DashboardSvg from './Components/Dashboard'
+import DeleteSvg from './Components/Delete'
 import DragSvg from './Components/Drag'
+import DuplicateSvg from './Components/Duplicate'
+import EditNameSvg from './Components/EditName'
 import EditSvg from './Components/Edit'
+import EmptyLineSvg from './Components/EmptyLine'
+import ExpandSvg from './Components/Expand'
 import GraphSvg from './Components/Graph'
+import HideSvg from './Components/Hide'
+import MoreSvg from './Components/More'
 import NewFlowSvg from './Components/NewFlow'
+import NewTabSvg from './Components/NewTab'
 import ResetSvg from './Components/Reset'
 import ScheduleSvg from './Components/Schedule'
+import ShowSvg from './Components/Show'
 import ShowsSvg from './Components/Shows'
 import SmartCopySvg from './Components/SmartCopy'
 import ToggleOffScreenSvg from './Components/ToggleOff'
 import ToggleOnSvg from './Components/ToggleOn'
+import UnlockSvg from './Components/Unlock'
 import WelcomeScreenSvg from './Components/WelcomeScreen'
 
 import { Color } from '@based/theme'
@@ -109,33 +122,59 @@ const wrapIcon = (
   }
 }
 
+const Add = wrapIcon(AddSvg)
 const ChevronLeft = wrapIcon(ChevronLeftSvg)
+const ChevronRight = wrapIcon(ChevronRightSvg)
+const Collapse = wrapIcon(CollapseSvg)
 const Dashboard = wrapIcon(DashboardSvg)
+const Delete = wrapIcon(DeleteSvg)
 const Drag = wrapIcon(DragSvg)
+const Duplicate = wrapIcon(DuplicateSvg)
 const Edit = wrapIcon(EditSvg)
+const EditName = wrapIcon(EditNameSvg)
+const EmptyLine = wrapIcon(EmptyLineSvg)
+const Expand = wrapIcon(ExpandSvg)
 const Graph = wrapIcon(GraphSvg)
+const Hide = wrapIcon(HideSvg)
+const More = wrapIcon(MoreSvg)
 const NewFlow = wrapIcon(NewFlowSvg)
+const NewTab = wrapIcon(NewTabSvg)
 const Reset = wrapIcon(ResetSvg)
 const Schedule = wrapIcon(ScheduleSvg)
+const Show = wrapIcon(ShowSvg)
 const Shows = wrapIcon(ShowsSvg)
 const SmartCopy = wrapIcon(SmartCopySvg)
 const ToggleOff = wrapIcon(ToggleOffScreenSvg)
 const ToggleOn = wrapIcon(ToggleOnSvg)
+const Unlock = wrapIcon(UnlockSvg)
 const WelcomeScreen = wrapIcon(WelcomeScreenSvg)
 
 const icons = {
+  Add,
   ChevronLeft,
+  ChevronRight,
+  Collapse,
   Dashboard,
+  Delete,
   Drag,
+  Duplicate,
   Edit,
+  EditName,
+  EmptyLine,
+  Expand,
   Graph,
+  Hide,
+  More,
   NewFlow,
+  NewTab,
   Reset,
   Schedule,
+  Show,
   Shows,
   SmartCopy,
   ToggleOff,
   ToggleOn,
+  Unlock,
   WelcomeScreen,
 }
 
@@ -151,17 +190,30 @@ const iconFromString = (str: string): null | FunctionComponent<IconProps> => {
 export {
   icons,
   iconFromString,
+  Add,
   ChevronLeft,
+  ChevronRight,
+  Collapse,
   Dashboard,
+  Delete,
   Drag,
+  Duplicate,
   Edit,
+  EditName,
+  EmptyLine,
+  Expand,
   Graph,
+  Hide,
+  More,
   NewFlow,
+  NewTab,
   Reset,
   Schedule,
+  Show,
   Shows,
   SmartCopy,
   ToggleOff,
   ToggleOn,
+  Unlock,
   WelcomeScreen,
 }
