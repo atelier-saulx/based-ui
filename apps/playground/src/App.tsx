ad8ad045285cd8d6cecda95f398e05d42c86deb4
Jim de Beer <beerdejim@gmail.com>
2021-01-19 12:12:45 +0100

    no

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -2,20 +2,19 @@ import React, { CSSProperties } from 'react'
 
 import { useTheme } from '@based/theme'
 
-import { Code } from '@based/ui'
+// import { Code } from '@based/ui'
 
 const Bla = ({ style }: { style?: CSSProperties }) => {
   return <div style={{ ...style, background: 'red' }}>gurky</div>
 }
 
-const  App = () => {  
+const App = () => {  
   useTheme()
 
   return (
     <div>
       blurx for nuno
       <Bla />
-      <Code />
     </div>
   )
 }
--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -1,5 +1,6 @@
-import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
 
+console.log('x')
+// x
 ReactDom.render(App(), document.getElementById('root'))
