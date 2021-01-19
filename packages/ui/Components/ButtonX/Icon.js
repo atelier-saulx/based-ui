01f3bca3984a6ca4d3a3c9a602c8ba44f0230f1a
Jim de Beer <beerdejim@gmail.com>
2021-01-19 17:03:35 +0100

    fix

--- packages/ui/Components/ButtonX/Icon.js
+++
@@ -1,7 +0,0 @@
-import { iconFromString } from '@based/icons'
-import React from 'react'
-
-export const Icon = ({ onClick, icon, tooltip }) => {
-  const Icon = iconFromString(icon)
-  return Icon ? <Icon onClick={onClick} /> : null
-}
