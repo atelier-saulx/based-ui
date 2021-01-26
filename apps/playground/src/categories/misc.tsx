062eb9f3fd69155bc075c8327dc768802d5b2ebe
Nuno Frade <nfrade.tribal@gmail.com>
2021-01-26 16:58:57 +0100

    Signle line on menuitems

--- apps/playground/src/categories/misc.tsx
+++ apps/playground/src/categories/misc.tsx
@@ -49,7 +49,8 @@ export default {
                   icon: 'Custom',
                 },
                 {
-                  title: 'Subitem 3',
+                  title:
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed ultrices est. Mauris tortor metus, fringilla eget turpis in, suscipit facilisis ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit id risus sed pharetra. Vestibulum ante velit, posuere eget auctor nec, scelerisque ut tortor. Duis rhoncus mauris tincidunt magna mattis pretium. Etiam sit amet ipsum quis justo condimentum vulputate ac sed eros. Vivamus pretium finibus leo ac suscipit. Cras sit amet tortor in augue ultrices fringilla non at metus. Nullam mattis eleifend nisi quis aliquam. Vestibulum a euismod nibh. Sed vitae ligula nulla.disable',
                   icon: 'Custom',
                 },
               ],
