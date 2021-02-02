e5f326c15eb4213afa07ed95a84aa7e4be16e223
Nuno Frade <nfrade.tribal@gmail.com>
2021-02-02 09:47:19 +0100

    Remove zoom when preloader finishes

--- packages/ui/Components/Preloader/index.tsx
+++ packages/ui/Components/Preloader/index.tsx
@@ -47,27 +47,15 @@ export const Preloader = ({ loading = false, children }) => {
             bottom: 0,
             left: 0,
             opacity: fontLoaded && !loading ? 0 : 1,
-            transition: 'opacity 0.75s, transform 0.5s',
+            transition: 'opacity 0.75s',
             right: 0,
-            // backgroundImage: `linear-gradient(135deg,${useColor(
-            //   'default',
-            //   0.05
-            // )} 0%,${useColor('background2', 0.1)} 100%)`,
             backgroundColor: useColor({ color: 'background' }),
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
           }}
         >
-          <Loader
-            fadeIn
-            delay={1200}
-            size={24}
-            style={{
-              transform: fontLoaded && !loading ? 'scale(2)' : 'scale(1)',
-              transition: 'transform 0.5s',
-            }}
-          />
+          <Loader fadeIn delay={1200} size={24} />
         </div>
       )}
     </>
