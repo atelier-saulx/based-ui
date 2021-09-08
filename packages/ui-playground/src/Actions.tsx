946f89b17d792e69cb09e7c0735f517945e9fe7a
youzi <youri@saulx.com>
2021-09-08 17:11:45 +0200

    check it out

--- packages/ui-playground/src/Actions.tsx
+++ packages/ui-playground/src/Actions.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Button, Select } from '@based/ui'
 import { updateTheme, Colors, getTheme } from '@based/theme'
 import { getLanguage, updateLanguage } from '@based/text'
-import langs from '@based/text/languages.json'
+import langs from '@based/text/dist/languages'
 
 const original = getTheme()
 const differentTheme: Colors = {
