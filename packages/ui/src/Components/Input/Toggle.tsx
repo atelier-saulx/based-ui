a144d5206be178a2ccd97c0e4b4ec50ffd2194cb
Jim de Beer <beerdejim@gmail.com>
2021-09-25 12:46:01 +0200

    no flex

--- packages/ui/src/Components/Input/Toggle.tsx
+++ packages/ui/src/Components/Input/Toggle.tsx
@@ -70,8 +70,6 @@ export const CheckBox: FunctionComponent<ToggleInputProps & CheckProps> = ({
           noSelect
           style={{
             marginLeft: 15,
-            display: 'flex',
-            alignItems: 'center',
           }}
         >
           {children}
@@ -133,8 +131,6 @@ export const RadioButton: FunctionComponent<ToggleInputProps> = ({
           noSelect
           style={{
             marginLeft: 15,
-            display: 'flex',
-            alignItems: 'center',
           }}
         >
           {children}
