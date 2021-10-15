da8ab00bedb18fb05d76c4b1635db9032df3f0da
Nuno Frade <nfrade.tribal@gmail.com>
2021-10-15 12:41:06 +0200

    Use MultiSectionInput for date and time inputs

--- packages/ui/src/Components/Input/Date.tsx
+++ packages/ui/src/Components/Input/Date.tsx
@@ -1,36 +1,17 @@
-import React, {
-  CSSProperties,
-  FunctionComponent,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-} from 'react'
-import { Color, useColor } from '@based/theme'
+import React, { FunctionComponent, useEffect, useRef, useState } from 'react'
 import { OnValueChange } from '../../types'
-import useHover from '../../hooks/events/useHover'
 import { Date as DateIcon } from '@based/icons'
-import Clear from './Clear'
-import { matchKeyCode, Key } from '../../hooks/events/useKeyboard'
 import {
   dateValueToTimestamp,
-  getSelectionBlockIndex,
-  parseDateString,
   timestampToDateString,
 } from './dateTimeFunctions'
 import { DatePickerOverlay } from './DatePickerOverlay'
 import useOverlay from '../../hooks/overlay/useOverlay'
+import { MultiSectionInput, MultiSectionInputProps } from './MultiSectionInput'
 
-type DateInputProps = {
-  border?: boolean
-  weight?: 'semibold' | 'medium' | 'regular'
-  style?: CSSProperties
-  value?: string | number
-  identifier?: any
-  color?: Color
+type DateInputProps = Omit<MultiSectionInputProps, 'icon'> & {
+  value: number
   onChange: OnValueChange<number | undefined>
-  noBackground?: boolean
-  noHover?: boolean
 }
 
 // TODO: handle tab when popup is open
@@ -45,20 +26,20 @@ export const DateInput: FunctionComponent<DateInputProps> = ({
   noHover,
   weight,
 }) => {
-  const [hover, isHover] = noHover ? [{}, false] : useHover()
-  const [isFocus, setFocus] = useState(false)
-  const inputRef = useRef<HTMLInputElement>()
-  const dateString = useRef(timestampToDateString(dateValueToTimestamp(value)))
-  const setDateString = (value: string): void => {
-    dateString.current = value
-    inputRef.current.value = dateString.current
-  }
-  const [datePickerDate, setDatePickerDate] = useState(
+  const [hasFocus, setHasFocus] = useState(false)
+  let [dateString, setDateString] = useState<string>(
+    timestampToDateString(Number(value))
+  )
+  // const setDateString = (v: string): void => {
+  //   dateString = v
+  // }
+  const [datePickerDate, setDatePickerDate] = useState<Date>(
     value ? new Date(value) : new Date()
   )
-  const update = (): void => {
-    onChange(dateValueToTimestamp(dateString.current))
-    setDatePickerDate(new Date(dateValueToTimestamp(dateString.current)))
+  const update = (v: string): void => {
+    onChange(dateValueToTimestamp(v))
+    console.log({ v })
+    setDatePickerDate(new Date(dateValueToTimestamp(dateString)))
   }
 
   const initialValue = useRef<string | number>()
@@ -74,198 +55,60 @@ export const DateInput: FunctionComponent<DateInputProps> = ({
     }
   }, [value, identifier])
 
-  const clear = useCallback(() => {
-    setDateString('')
-    onChange(null)
-  }, [onChange])
-
-  const blocks = [
-    [0, 2],
-    [3, 5],
-    [6, 11],
-  ]
-
   return (
-    <div
-      style={{
-        width: 200,
-      }}
-    >
-      <div
-        {...hover}
-        style={{
-          position: 'relative',
-
-          paddingLeft: border && isFocus ? 11 : 12,
-          paddingRight: border && isFocus ? 11 : 12,
-          display: 'flex',
-          alignItems: 'center',
-          borderRadius: '4px',
-          flexGrow: 1,
-          background: noBackground
-            ? 'transparent'
-            : useColor({
-                color: color.color,
-                tone: isFocus || isHover ? color.tone + 1 : 1,
-              }),
-          border: border
-            ? isFocus
-              ? '2px solid ' + useColor({ color: 'primary' })
-              : '1px solid ' +
-                useColor({
-                  color: 'divider',
-                  opacity: border ? 1 : 0,
-                })
-            : null,
-          ...style,
-        }}
-      >
-        <>
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              bottom: 0,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <DateIcon color={{ color: 'foreground', tone: 4 }} />
-          </div>
-          <div style={{ width: 28 }} />
-        </>
-        <input
-          ref={inputRef}
-          placeholder="dd/mm/yyyy"
-          style={{
-            width: '100%',
-            paddingLeft: 6.5,
-            paddingTop: isFocus && border ? 6.5 : 7.5,
-            paddingBottom: isFocus && border ? 6.5 : 7.5,
-            appearance: 'none',
-            fontSize: '15px',
-            lineHeight: '24px',
-            letterSpacing: '-0.015em',
-            background: 'none',
-            fontFamily: 'Font',
-            color: useColor({ color: 'foreground' }),
-            userSelect: 'all',
-            fontWeight:
-              weight === 'semibold'
-                ? 600
-                : weight === 'medium'
-                ? 500
-                : 'normal',
-          }}
-          onFocus={useOverlay(
-            DatePickerOverlay,
-            {
-              date: datePickerDate,
-              align: 'flex-end',
-              width: 260,
-              onChange: (newDate) => {
-                setDateString(timestampToDateString(newDate.getTime()))
-                update()
-              },
-            },
-            () => {
-              setFocus(true)
-              return () => {
-                setFocus(false)
-              }
-            }
-          )}
-          onBlur={(e) => {
-            const { value } = e.target as HTMLInputElement
-            setFocus(false)
-            const parsedDateString = parseDateString(value)
-            if (parsedDateString.valid) {
-              setDateString(timestampToDateString(parsedDateString.timestamp))
-            }
-          }}
-          onChange={useCallback((e) => {
-            const { value } = e.target as HTMLInputElement
-            const parsedDateString = parseDateString(value)
-            setDateString(parsedDateString.formatedString)
-          }, [])}
-          onClick={(e) => {
-            const el = e.target as HTMLInputElement
-            const blockIndex = getSelectionBlockIndex(el, blocks)
-            el.setSelectionRange(blocks[blockIndex][0], blocks[blockIndex][1])
-          }}
-          onKeyDown={(e) => {
-            const el = e.target as HTMLInputElement
-
-            const key = [
-              'ArrowLeft',
-              'ArrowRight',
-              'ArrowUp',
-              'ArrowDown',
-            ].find((k: Key) => matchKeyCode(k, e as any))
-            if (key) {
-              e.preventDefault()
-              e.stopPropagation()
-
-              const blockIndex = getSelectionBlockIndex(el, blocks)
-
-              if (key === 'ArrowLeft' && blockIndex > 0) {
-                el.setSelectionRange(
-                  blocks[blockIndex - 1][0],
-                  blocks[blockIndex - 1][1]
-                )
-                return
-              } else if (
-                key === 'ArrowRight' &&
-                blockIndex < blocks.length - 1
-              ) {
-                el.setSelectionRange(
-                  blocks[blockIndex + 1][0],
-                  blocks[blockIndex + 1][1]
-                )
-                return
-              } else if (key === 'ArrowUp' || key === 'ArrowDown') {
-                const pre = el.value.substring(0, blocks[blockIndex][0])
-                const value = el.value.substring(
-                  blocks[blockIndex][0],
-                  blocks[blockIndex][1]
-                )
-                let newValue
-                if (key === 'ArrowUp') {
-                  newValue = parseInt(value) + 1
-                  if (
-                    (blockIndex === 0 && newValue > 31) ||
-                    (blockIndex === 1 && newValue > 12)
-                  ) {
-                    newValue = 1
-                  }
-                } else {
-                  newValue = parseInt(value) - 1
-                  if (newValue < 1) {
-                    newValue = blockIndex === 0 ? 31 : blockIndex === 1 ? 12 : 1
-                  }
-                }
-                const post = el.value.substring(blocks[blockIndex][1])
-                setDateString(
-                  pre +
-                    ('0' + newValue).slice(blockIndex === 2 ? -4 : -2) +
-                    post
-                )
-                update()
-              }
-
-              el.setSelectionRange(blocks[blockIndex][0], blocks[blockIndex][1])
-            }
-          }}
-        />
-        <Clear
-          style={{
-            // @ts-ignore
-            opacity: isHover && dateString ? 1 : 0,
-          }}
-          onClick={clear}
-        />
-      </div>
-    </div>
+    <MultiSectionInput
+      onChange={update}
+      onChangeStrategy="onChangeAndValid"
+      border={border}
+      value={dateString}
+      style={style}
+      noBackground={noBackground}
+      color={color}
+      weight={weight}
+      noHover={noHover}
+      icon={DateIcon}
+      hasFocus={hasFocus}
+      placeholder="dd/mm/yyyy"
+      onFocus={useOverlay(
+        DatePickerOverlay,
+        {
+          date: datePickerDate,
+          align: 'flex-end',
+          width: 260,
+          onChange: (newDate) => {
+            setDateString(timestampToDateString(dateValueToTimestamp(newDate)))
+            update(timestampToDateString(dateValueToTimestamp(newDate)))
+          },
+        },
+        () => {
+          setHasFocus(true)
+          return () => {
+            setHasFocus(false)
+          }
+        }
+      )}
+      sections={[
+        {
+          validation: /^(\d|[0-2]\d|3[0-1])$/,
+          maxSize: 2,
+          preprocess: (v) => ('00' + v).substr(-2),
+          separator: '/',
+          default: '00',
+        },
+        {
+          validation: /^(\d|1[0-2])$/,
+          maxSize: 2,
+          preprocess: (v) => ('00' + v).substr(-2),
+          separator: '/',
+          default: '00',
+        },
+        {
+          validation: /^([1-2]\d{3})$/,
+          maxSize: 4,
+          preprocess: (v) => v,
+          default: '2001',
+        },
+      ]}
+    />
   )
 }
--- packages/ui/src/Components/Input/Time.tsx
+++ packages/ui/src/Components/Input/Time.tsx
@@ -1,35 +1,16 @@
-import React, {
-  CSSProperties,
-  FunctionComponent,
-  useCallback,
-  useRef,
-  useState,
-  useEffect,
-} from 'react'
-import { Color, useColor } from '@based/theme'
+import React, { FunctionComponent, useRef, useEffect } from 'react'
 import { OnValueChange } from '../../types'
-import useHover from '../../hooks/events/useHover'
 import { Time } from '@based/icons'
-import Clear from './Clear'
-import { matchKeyCode, Key } from '../../hooks/events/useKeyboard'
 import {
-  parseTimeString,
   milisecondsToTimeString,
   timeValueToMiliseconds,
-  getSelectionBlockIndex,
 } from './dateTimeFunctions'
+import { MultiSectionInput, MultiSectionInputProps } from './MultiSectionInput'
+import { Section } from './MultiSectionInputFunctions'
 
-type TimeInputProps = {
-  border: boolean
-  weight?: 'semibold' | 'medium' | 'regular'
-  style?: CSSProperties
-  color?: Color
-  value?: string | number
-  identifier?: any
+type TimeInputProps = Omit<MultiSectionInputProps, 'icon'> & {
   onChange: OnValueChange<number | undefined>
   useSeconds?: boolean
-  noBackground?: boolean
-  noHover?: boolean
 }
 
 /**
@@ -52,18 +33,17 @@ export const TimeInput: FunctionComponent<TimeInputProps> = ({
   noHover,
   weight,
 }) => {
-  const [hover, isHover] = noHover ? [{}, false] : useHover()
-  const [isFocus, setFocus] = useState(false)
-  const inputRef = useRef<HTMLInputElement>()
-  const timeString = useRef(
+  // const timeString = useRef(
+  //   milisecondsToTimeString(timeValueToMiliseconds(value), useSeconds) || ''
+  // )
+  let timeString =
     milisecondsToTimeString(timeValueToMiliseconds(value), useSeconds) || ''
-  )
-  const setTimeString = (value: string): void => {
-    timeString.current = value
-    inputRef.current.value = timeString.current
+
+  const setTimeString = (v: string): void => {
+    timeString = v
   }
-  const update = () => {
-    onChange(timeValueToMiliseconds(timeString.current))
+  const update = (v: string) => {
+    onChange(timeValueToMiliseconds(v))
   }
 
   const initialValue = useRef<string | number>()
@@ -81,180 +61,44 @@ export const TimeInput: FunctionComponent<TimeInputProps> = ({
     }
   }, [value, identifier])
 
-  const clear = useCallback(() => {
-    setTimeString('')
-    onChange(null)
-  }, [onChange])
-
-  const blocks = [
-    [0, 2],
-    [3, 5],
+  let sections: Section[] = [
+    {
+      validation: /^(\d|[0-1]\d|2[0-3])$/,
+      maxSize: 2,
+      preprocess: (v) => ('00' + v).substr(-2),
+      separator: ':',
+      default: '00',
+    },
+    {
+      validation: /^(\d|[0-5]\d|2\d)$/,
+      maxSize: 2,
+      preprocess: (v) => ('00' + v).substr(-2),
+      separator: ':',
+      default: '00',
+    },
   ]
-  if (useSeconds) blocks.push([6, 9])
+  if (useSeconds) {
+    sections.push({
+      validation: /^(\d|[0-5]\d|2\d)$/,
+      maxSize: 2,
+      preprocess: (v) => ('00' + v).substr(-2),
+      default: '00',
+    })
+  }
 
   return (
-    <div
-      style={{
-        width: 200,
-      }}
-    >
-      <div
-        {...hover}
-        style={{
-          position: 'relative',
-          paddingLeft: border && isFocus ? 11 : 12,
-          paddingRight: border && isFocus ? 11 : 12,
-          display: 'flex',
-          alignItems: 'center',
-          borderRadius: '4px',
-          flexGrow: 1,
-          background: noBackground
-            ? 'transparent'
-            : useColor({
-                color: color.color,
-                tone: isFocus || isHover ? color.tone + 1 : 1,
-              }),
-          border: border
-            ? isFocus
-              ? '2px solid ' + useColor({ color: 'primary' })
-              : '1px solid ' +
-                useColor({
-                  color: 'divider',
-                  opacity: border ? 1 : 0,
-                })
-            : null,
-          ...style,
-        }}
-      >
-        <>
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              bottom: 0,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Time color={{ color: 'foreground', tone: 4 }} />
-          </div>
-          <div style={{ width: 28 }} />
-        </>
-        <input
-          ref={inputRef}
-          placeholder={useSeconds ? 'hh:mm:ss' : 'hh:mm'}
-          style={{
-            width: '100%',
-            paddingLeft: 6.5,
-            paddingTop: isFocus && border ? 6.5 : 7.5,
-            paddingBottom: isFocus && border ? 6.5 : 7.5,
-            appearance: 'none',
-            fontSize: '15px',
-            lineHeight: '24px',
-            letterSpacing: '-0.015em',
-            background: 'none',
-            fontFamily: 'Font',
-            color: useColor({ color: 'foreground' }),
-            userSelect: 'all',
-            fontWeight:
-              weight === 'semibold'
-                ? 600
-                : weight === 'medium'
-                ? 500
-                : 'normal',
-          }}
-          onFocus={() => setFocus(true)}
-          onBlur={(e) => {
-            const { value } = e.target as HTMLInputElement
-            const parsedTimeString = parseTimeString(value, useSeconds)
-            if (parsedTimeString.milliseconds) {
-              setTimeString(parsedTimeString.formatedString)
-              update()
-            }
-            setFocus(false)
-          }}
-          onChange={useCallback((e) => {
-            const { value } = e.target as HTMLInputElement
-            const parsedTimeString = parseTimeString(value, useSeconds)
-            setTimeString(parsedTimeString.formatedString)
-            if (parsedTimeString.valid) {
-              update()
-            }
-          }, [])}
-          onClick={(e) => {
-            const el = e.target as HTMLInputElement
-            const blockIndex = getSelectionBlockIndex(el, blocks)
-            el.setSelectionRange(blocks[blockIndex][0], blocks[blockIndex][1])
-          }}
-          onKeyDown={(e) => {
-            const el = e.target as HTMLInputElement
-
-            const key = [
-              'ArrowLeft',
-              'ArrowRight',
-              'ArrowUp',
-              'Enter',
-              'ArrowDown',
-            ].find((k: Key) => matchKeyCode(k, e as any))
-            if (key) {
-              e.preventDefault()
-              e.stopPropagation()
-
-              const blockIndex = getSelectionBlockIndex(el, blocks)
-
-              if (key === 'Enter') {
-                onChange(timeValueToMiliseconds(timeString.current))
-              } else if (key === 'ArrowLeft' && blockIndex > 0) {
-                el.setSelectionRange(
-                  blocks[blockIndex - 1][0],
-                  blocks[blockIndex - 1][1]
-                )
-                return
-              } else if (
-                key === 'ArrowRight' &&
-                blockIndex < blocks.length - 1
-              ) {
-                el.setSelectionRange(
-                  blocks[blockIndex + 1][0],
-                  blocks[blockIndex + 1][1]
-                )
-                return
-              } else if (key === 'ArrowUp' || key === 'ArrowDown') {
-                const pre = el.value.substring(0, blocks[blockIndex][0])
-                const value = el.value.substring(
-                  blocks[blockIndex][0],
-                  blocks[blockIndex][1]
-                )
-                let newValue
-                if (key === 'ArrowUp') {
-                  newValue = parseInt(value) + 1
-                  if ((blockIndex === 0 && newValue > 23) || newValue > 59) {
-                    newValue = 0
-                  }
-                } else {
-                  newValue = parseInt(value) - 1
-                  if (newValue < 0) {
-                    newValue = blockIndex === 0 ? 23 : 59
-                  }
-                }
-                const post = el.value.substring(blocks[blockIndex][1])
-                setTimeString(pre + ('0' + newValue).slice(-2) + post)
-                update()
-              }
-
-              el.setSelectionRange(blocks[blockIndex][0], blocks[blockIndex][1])
-            }
-          }}
-        />
-        <Clear
-          style={{
-            // @ts-ignore
-            opacity: isHover && timeString ? 1 : 0,
-          }}
-          onClick={clear}
-        />
-      </div>
-    </div>
+    <MultiSectionInput
+      onChange={update}
+      border={border}
+      value={timeString}
+      style={style}
+      noBackground={noBackground}
+      color={color}
+      weight={weight}
+      noHover={noHover}
+      icon={Time}
+      placeholder={useSeconds ? 'hh:mm:ss' : 'hh:mm'}
+      sections={sections}
+    />
   )
 }
