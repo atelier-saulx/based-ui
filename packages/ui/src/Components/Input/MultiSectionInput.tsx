b63695e70ff00d5fb7477043cb2a089b620946ec
Nuno Frade <nfrade.tribal@gmail.com>
2021-10-18 13:29:42 +0200

    Cleanup

--- packages/ui/src/Components/Input/MultiSectionInput.tsx
+++ packages/ui/src/Components/Input/MultiSectionInput.tsx
@@ -65,14 +65,6 @@ export type MultiSectionInputProps = {
 //   default: '00',
 // },
 
-/**
- * Time inpuut field
- * WARNING: Accepts and returns miliseconds.
- * If milliseconds are used as timestamp to a Date object it will add local timezone offset
- * when getting or viewing value.
- * UTC functions must be used when setting or getting hours/minustes/seconds:
- * setUTCHours(), getUTC*(), toUTCString()
- */
 export const MultiSectionInput: FunctionComponent<MultiSectionInputProps> = ({
   border,
   value,
