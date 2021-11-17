60b1e3d38087c36de56d6364291246795fba19fd
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-11-17 10:06:17 +0100

    refactor: Extract functionality

--- packages/ui/src/hooks/overlay/useDropdown.tsx
+++ packages/ui/src/hooks/overlay/useDropdown.tsx
@@ -41,6 +41,47 @@ export default (
     ...props,
   })
 
+  function onDropdownChange(option, index, event, dropdown) {
+    if (context.current.props.multi) {
+      let value = context.current.props.value
+      if (!Array.isArray(value)) {
+        value = []
+      }
+
+      const index = findOptionIndex(value, option)
+      value = [...value]
+
+      if (index !== -1) {
+        value.splice(index, 1)
+      } else {
+        value.push(option)
+      }
+
+      const response = onSelect(
+        value,
+        value.map((selectValue) =>
+          findOptionIndex(context.current.props.items, selectValue)
+        ),
+        event
+      )
+
+      if (Array.isArray(response)) {
+        value = response
+      }
+
+      context.current.update({ ...context.current.props, value })
+    } else {
+      removeOverlay(dropdown)
+
+      context.current.update({
+        ...context.current.props,
+        value: option,
+      })
+
+      onSelect(option, index, event)
+    }
+  }
+
   return useCallback(
     (event, extraProps) => {
       event.preventDefault()
@@ -54,44 +95,9 @@ export default (
             // @ts-ignore
             target={event.currentTarget}
             items={items}
-            onChange={(option, index, event) => {
-              if (context.current.props.multi) {
-                let value = context.current.props.value
-                if (!Array.isArray(value)) {
-                  value = []
-                }
-
-                const index = findOptionIndex(value, option)
-                value = [...value]
-
-                if (index !== -1) {
-                  value.splice(index, 1)
-                } else {
-                  value.push(option)
-                }
-
-                const response = onSelect(
-                  value,
-                  value.map((selectValue) =>
-                    findOptionIndex(context.current.props.items, selectValue)
-                  ),
-                  event
-                )
-
-                if (Array.isArray(response)) {
-                  value = response
-                }
-
-                context.current.update({ ...context.current.props, value })
-              } else {
-                removeOverlay(dropdown)
-                context.current.update({
-                  ...context.current.props,
-                  value: option,
-                })
-                onSelect(option, index, event)
-              }
-            }}
+            onChange={(option, index, event) =>
+              onDropdownChange(option, index, event, dropdown)
+            }
             {...props}
             {...extraProps}
           />
