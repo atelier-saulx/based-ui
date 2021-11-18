1276842d77fd20f693e7844bc7bf2cf74551243f
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-11-18 11:43:38 +0100

    feat: Add example file from ui-next

--- packages/docs/pages/index.tsx
+++ packages/docs/pages/index.tsx
@@ -1,7 +1,27 @@
 import type { NextPage } from 'next'
 
+import { Example } from '../../ui-next/dist'
+
 const Home: NextPage = () => {
-  return <div>Hello success</div>
+  return (
+    <div>
+      <div
+        style={{
+          width: 'auto',
+          maxWidth: '800px',
+          height: 'auto',
+          margin: '0 auto',
+          marginTop: '40px',
+        }}
+      >
+        <h1>Playground</h1>
+
+        <div>
+          <Example></Example>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
