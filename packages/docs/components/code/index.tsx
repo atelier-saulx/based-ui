f7401bf27afbb911e85b7aec9f6be926b0e2b8f7
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-11-23 23:27:18 +0100

    feat: Primitive code copy

--- packages/docs/components/code/index.tsx
+++ packages/docs/components/code/index.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useEffect } from 'react'
 import Prism from 'prismjs'
 import 'prismjs/themes/prism-tomorrow.css'
+import { useSetup } from './setup'
 
 export type CodeProps = {
   code: string
@@ -12,8 +13,10 @@ export const Code: FunctionComponent<CodeProps> = ({ code, language }) => {
     Prism.highlightAll()
   }, [])
 
+  useSetup()
+
   return (
-    <pre>
+    <pre data-toolbar-order="select-code,copy-code">
       <code className={`language-${language}`}>{code}</code>
     </pre>
   )
